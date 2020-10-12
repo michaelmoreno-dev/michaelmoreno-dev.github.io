4b60c833aa8cd76727ca4562299d18cb710dc6a1
Michael Moreno <michaelmoreno2k@gmail.com>
2020-10-12 11:40:55 -0500

    Using clone method for pawns

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $('<div>').attr('id','board').appendTo('body');
-let color = 0;
+let color = 1;
 for (i = 8; i >= 1; i--) {
   $('<div>').addClass(`rank rank-${i}`).appendTo('#board');
   color++
@@ -14,11 +14,27 @@ for (i = 8; i >= 1; i--) {
   }
 }
 
-$(`.rank-2 .file-1`).html('<img src="./styles/imgs/pawn.svg">')
-$(`.rank-2 .file-2`).html('<img src="./styles/imgs/pawn.svg">')
-$(`.rank-2 .file-3`).html('<img src="./styles/imgs/pawn.svg">')
-$(`.rank-2 .file-4`).html('<img src="./styles/imgs/pawn.svg">')
-$(`.rank-2 .file-5`).html('<img src="./styles/imgs/pawn.svg">')
-$(`.rank-2 .file-6`).html('<img src="./styles/imgs/pawn.svg">')
-$(`.rank-2 .file-7`).html('<img src="./styles/imgs/pawn.svg">')
-$(`.rank-2 .file-8`).html('<img src="./styles/imgs/pawn.svg">')
+// PIECES
+  // WHITE PIECES
+
+const $whitePawn = $('<img src="./styles/imgs/whitepawn.svg">').addClass('pawn white-pawn')
+
+$(`.rank-2 .file-1`).append($whitePawn.clone());
+$(`.rank-2 .file-2`).append($whitePawn.clone());
+$(`.rank-2 .file-3`).append($whitePawn.clone());
+$(`.rank-2 .file-4`).append($whitePawn.clone());
+$(`.rank-2 .file-5`).append($whitePawn.clone());
+$(`.rank-2 .file-6`).append($whitePawn.clone());
+$(`.rank-2 .file-7`).append($whitePawn.clone());
+$(`.rank-2 .file-8`).append($whitePawn.clone());
+
+const $blackPawn = $('<img src="./styles/imgs/blackpawn.svg">').addClass('pawn white-pawn')
+
+$(`.rank-7 .file-1`).append($blackPawn.clone());
+$(`.rank-7 .file-2`).html('<img src="./styles/imgs/blackpawn.svg">')
+$(`.rank-7 .file-3`).html('<img src="./styles/imgs/blackpawn.svg">')
+$(`.rank-7 .file-4`).html('<img src="./styles/imgs/blackpawn.svg">')
+$(`.rank-7 .file-5`).html('<img src="./styles/imgs/blackpawn.svg">')
+$(`.rank-7 .file-6`).html('<img src="./styles/imgs/blackpawn.svg">')
+$(`.rank-7 .file-7`).html('<img src="./styles/imgs/blackpawn.svg">')
+$(`.rank-7 .file-8`).html('<img src="./styles/imgs/blackpawn.svg">')
