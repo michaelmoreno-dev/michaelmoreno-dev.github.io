732714443f0ff92f9e70e6d4dbb0e52b5c697f5f
michaelmoreno.dev@gmail.com <michaelmoreno.dev@gmail.com>
2020-10-13 11:46:00 -0500

    Made same-file, not-same row, and distance limits with turn option as one condition

--- app.js
+++ app.js
@@ -76,54 +76,37 @@ function select() {
     if ($this.children().length > 1) {
       $this.css({'border':'5px solid cyan', 'width':'90px', 'height':'90px'});
       var current = {
-        $rank: $this.parent().attr('class').split(' ')[1].split('-')[1],
-        $file: $this.attr('class').split(' ')[1].split('-')[1],
+        $file: parseInt($this.attr('class').split(' ')[1].split('-')[1]),
+        $rank: parseInt($this.parent().attr('class').split(' ')[1].split('-')[1]),
         $piece: $this.children().eq(1),
       }
       // Target Square
       $('.file').one('click',function(){
         let $this = $(this);
         let target = {
-          $rank: $this.parent().attr('class').split(' ')[1].split('-')[1],
-          $file: $this.attr('class').split(' ')[1].split('-')[1],
+          $file: parseInt($this.attr('class').split(' ')[1].split('-')[1]),
+          $rank: parseInt($this.parent().attr('class').split(' ')[1].split('-')[1]),
           $piece: $this.children().eq(1),
         }
         $('.file').off('click');
         // IF PIECE IS PAWN
         if (current.$piece.attr('class').split(' ')[1] === 'pawn') {
           // IF FIRST TURN
-          if (turn == 0) {
-            alert(turn);
-            if (current.$file == target.$file && Math.abs(current.$rank - target.$rank) < 10) {
-              alert('less than 2')
-              if ($this.children().length > 1) {
-                alert('piece present')
-                if (target.$piece.attr('class').split(' ')[0] == 'black') {
-                  alert('free');
-                  target.$piece.appendTo('.black-graveyard');
-                  current.$piece.appendTo($this);
-                  turn++
-                }
-                else {
-                  alert('Ally piece blocking!');
-                }
-              }
-            }
-            else {
-              alert('Your pawn cannot move that far!')
-            }
-          }
-          else {
-            alert(turn);
-            if (current.$file == target.$file && Math.abs(current.$rank - target.$rank) < 10) {
-              alert('work');
+          if (current.$file == target.$file && current.$rank !== target.$rank && (Math.abs(current.$rank - target.$rank) < 2 || (Math.abs(current.$rank - target.$rank) < 3 && turn == 0))) {
+            alert('worked');
+            if ($this.children().length < 2) {
               current.$piece.appendTo($this);
               turn++;
             }
-            else {
-              alert('Your pawn cannot move that far!')
-            }
           }
+          else {
+            alert('failed');
+          }
+          console.log(`file: .${current.$file + 1} rank: .${current.$rank + 1}`);
+          // else if ($(`.${current.$rank+1} .${current.$file+1}`))
+          // else {
+          //   alert('Your pawn cannot move that far!')
+          // }
         }
         // if ($this.children().length > 1) {
         //   if (target.$piece.attr('class').split(' ')[0] === 'black') {
